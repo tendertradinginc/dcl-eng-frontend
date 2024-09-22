a96784d18a1517e5381997ff08b1c7c17583f974
husneara <husnearaa99@gmail.com>
2024-09-22 15:20:56 +0600

    has complete initial setup

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button';
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      <h1>hello legendary</h1>
+      <Button className="text-xl text-blue-600 " variant="outline">CLICK ME</Button>
+
+    </div>
+  );
+};
+
+export default page;
