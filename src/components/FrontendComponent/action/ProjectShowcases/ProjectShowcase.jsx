dbba3975970cb8752045f3a70dec465d64b54a62
husneara <husnearaa99@gmail.com>
2024-10-02 16:30:54 +0600

    has added title section

--- src/components/FrontendComponent/action/ProjectShowcases/ProjectShowcase.jsx
+++ src/components/FrontendComponent/action/ProjectShowcases/ProjectShowcase.jsx
@@ -1,5 +1,6 @@
 "use client"
 
+import MaxWidthWrapper from '@/components/custom/MaxWidthWrapper';
 import PageBanner from '@/components/shared/PageBanner/PageBanner';
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from '@/components/ui/breadcrumb';
 import Link from 'next/link';
@@ -27,6 +28,28 @@ const ProjectShowcases = () => {
                     </BreadcrumbList>
                 </Breadcrumb></>} />
             </div>
+
+            <MaxWidthWrapper>
+                {/* Section Heading with background */}
+                <div className="relative z-10 text-center mt-6 mb-8 py-12">
+                    {/* Background with "success" watermark behind the heading only */}
+                    <div className="absolute inset-0 flex justify-center items-center z-0 -mt-24"> {/* Adjusted margin to -28 */}
+                        <p className="text-8xl font-bold text-gray-300 opacity-30">Showcase</p>
+                    </div>
+
+                    {/* Section Heading */}
+                    <div className="relative z-10 mt-4 ">
+                        <p className="text-base text-orange-500">Show Our Projects -</p>
+                        <h2 className="lg:text-3xl text-xl font-bold mt-2 text-black inline-block relative text-center">
+                            PROJECT
+                            <span className="relative inline-block ml-2">
+                                <span className="font-bold text-black">SHOWCASE</span>
+                                <span className="block h-1 w-full bg-orange-500 absolute bottom-0 left-0"></span>
+                            </span>
+                        </h2>
+                    </div>
+                </div>
+            </MaxWidthWrapper>
 
         </div>
     );
