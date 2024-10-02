556ee6082ca97123980bfa22a54252a0a219a5e1
sakil-sagor <sakilsagor76543@gmail.com>
2024-10-02 13:29:48 +0600

    create shared component for section title and page title

---
+++ src/components/shared/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+const SectionTitle = ({ title, imageLink, position, width, height }) => {
+  return (
+    <div className={`flex flex-col ${[position]} mb-12`}>
+      <div className="mb-5 z-10 ">
+        <Image
+          alt="our vision imave"
+          height={height}
+          width={width}
+          className="w-[300px] md:w-[637px]"
+          src={imageLink}
+        ></Image>
+      </div>
+      <div className="flex items-center gap-3 -mt-14 ml-4">
+        {" "}
+        <p className="text-[#F78C40] inline-block text-center">{title}</p>
+        <p className="h-1 w-16 bg-gradient-to-tr from-[#ffc1957e] to-[#F78C40]"></p>
+      </div>
+    </div>
+  );
+};
+
+export default SectionTitle;
