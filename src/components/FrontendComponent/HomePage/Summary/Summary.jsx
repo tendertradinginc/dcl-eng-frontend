4f6148d149d2420180d70094daf61785d3dd392c
sakil-sagor <sakilsagor76543@gmail.com>
2024-10-02 23:55:32 +0600

    complete summary

--- src/components/FrontendComponent/HomePage/Summary/Summary.jsx
+++ src/components/FrontendComponent/HomePage/Summary/Summary.jsx
@@ -1,8 +1,10 @@
+"use client";
 import MaxWidthWrapper from "@/components/custom/MaxWidthWrapper";
+import CountUp from "react-countup";
 
 const Summary = () => {
   return (
-    <div>
+    <div className="my-32">
       <MaxWidthWrapper>
         <div className="flex items-center gap-3">
           {" "}
@@ -24,10 +26,78 @@ const Summary = () => {
               </span>
             </div>
             <div className="flex items-center mt-[-80px] ml-28">
-              <p className="text-7xl font-bold">34</p>
+              <p className="text-7xl font-bold">
+                <CountUp end={34} duration={4} />
+              </p>
               <div className="ml-4">
                 <span className=" uppercase">Projects</span>
                 <p className="text-3xl font-semibold">Design</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div>
+              <span
+                className="text-transparent stroke-2 stroke-gray-300 outline-0 text-9xl font-extrabold"
+                style={{
+                  WebkitTextStroke: "2px #d1d5db",
+                  WebkitTextFillColor: "transparent",
+                }}
+              >
+                97
+              </span>
+            </div>
+            <div className="flex items-center mt-[-80px] ml-28">
+              <p className="text-7xl font-bold">
+                <CountUp end={97} duration={4} />
+              </p>
+              <div className="ml-4">
+                <span className=" uppercase">Happy</span>
+                <p className="text-3xl font-semibold">Client</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div>
+              <span
+                className="text-transparent stroke-2 stroke-gray-300 outline-0 text-9xl font-extrabold"
+                style={{
+                  WebkitTextStroke: "2px #d1d5db",
+                  WebkitTextFillColor: "transparent",
+                }}
+              >
+                15
+              </span>
+            </div>
+            <div className="flex items-center mt-[-80px] ml-28">
+              <p className="text-7xl font-bold">
+                <CountUp end={15} duration={4} />
+              </p>
+              <div className="ml-4">
+                <span className=" uppercase">Completed</span>
+                <p className="text-3xl font-semibold">Project</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div>
+              <span
+                className="text-transparent stroke-2 stroke-gray-300 outline-0 text-9xl font-extrabold"
+                style={{
+                  WebkitTextStroke: "2px #d1d5db",
+                  WebkitTextFillColor: "transparent",
+                }}
+              >
+                62
+              </span>
+            </div>
+            <div className="flex items-center mt-[-80px] ml-28">
+              <p className="text-7xl font-bold">
+                <CountUp end={62} duration={4} />
+              </p>
+              <div className="ml-4">
+                <span className=" uppercase">Success</span>
+                <p className="text-3xl font-semibold">Story</p>
               </div>
             </div>
           </div>
