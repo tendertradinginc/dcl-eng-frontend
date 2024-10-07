6f447bf10366d28ac094915af6ac4fe05cdec12d
frustrated018 <niloyd018@gmail.com>
2024-10-08 00:03:00 +0530

    Fixed the service page data fetching issue and updated the contact us form to make it active

--- src/app/(withDashboardLayout)/dashboard/service-page/page.jsx
+++ src/app/(withDashboardLayout)/dashboard/service-page/page.jsx
@@ -54,7 +54,7 @@ const Page = () => {
       setLoading(true);
       try {
         const response = await axios.get(
-          `http://localhost:5000/api/v1/service?page=${selectedPage}&limit=${selectedLimit}&search=${debouncedSearchTerm}`
+          `http://localhost:5000/api/v1/service/get-all-dashboard-edition?page=${selectedPage}&limit=${selectedLimit}&search=${debouncedSearchTerm}`
         );
 
         setData(response.data.data);
